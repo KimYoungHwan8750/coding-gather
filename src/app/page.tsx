4ac141253070eaa34f8f0452970b480cf1f27036
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-20 23:53:49 +0900

    분리되어있던 Websocket Event Listener를 Websocket Provider로 통합

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,79 +1,41 @@
-"use client";
-import { ResizablePanelGroup, ResizablePanel, ResizableHandle } from "@/components/ui/resizable";
-import Canvas from "./canvas";
-import TextEditor from "./editor";
-import { useState, useEffect, createContext, ReactNode, useContext } from "react";
-import { socket } from "../socket";
+import { WebSocketProvider } from "@/provider/websocket-provider";
+import { ReactNode } from "react";
+import Canvas from "@/app/canvas";
+import TextEditor from "@/app/editor";
+import { EditorFeatureProvider } from "@/provider/editor-provider";
+import { SharingAreaProvider } from "@/provider/sharing-area-provider";
 
-type wsContextType = {
-  socket: typeof socket
-}
-const WsContext = createContext<wsContextType | null>(null);
-export function useWs() {
-  const context = useContext(WsContext);
-  if (!context) {
-    throw new Error("useWs는 반드시 wsContext 하위에서 사용되어야 합니다.");
-  }
-  return context;
-}
-const WebSocketProvider = ({children}: {children: ReactNode}) => {
-  const [isConnected, setIsConnected] = useState(false);
-  const [transport, setTransport] = useState("N/A");
-  useEffect(() => {
-    if (socket.connected) {
-      onConnect();
-    }
-
-    function onConnect() {
-      setIsConnected(true);
-      setTransport(socket.io.engine.transport.name);
-      console.log("connected");
-
-      socket.io.engine.on("upgrade", (transport: any) => {
-        setTransport(transport.name);
-      });
-    }
-
-    function onDisconnect() {
-      setIsConnected(false);
-      setTransport("N/A");
-    }
-
-    socket.on("connect", onConnect);
-    socket.on("disconnect", onDisconnect);
-
-    return () => {
-      socket.off("connect", onConnect);
-      socket.off("disconnect", onDisconnect);
-    };
-  }, []);
-  return <WsContext.Provider value={{socket}}>{children}</WsContext.Provider>;
-}
 export default function Home() {
   return (
-    <WebSocketProvider>
-      <div className="w-screen h-screen">
-        <ResizablePanelGroup
-          direction="horizontal"
-          className="rounded-lg border w-screen"
-        >
-          <ResizablePanel defaultSize={50}>
+    <SharingAreaProvider>
+      <EditorFeatureProvider>
+        <WebSocketProvider>
+          <AppContainer>
             <Canvas></Canvas>
-          </ResizablePanel>
-          <ResizableHandle />
-          <ResizablePanel defaultSize={50}>
-            <ResizablePanelGroup direction="vertical">
-              <ResizablePanel defaultSize={25}>
-                <TextEditor direction="top"></TextEditor>
-              </ResizablePanel>
-              <ResizableHandle />
-              <ResizablePanel defaultSize={75}>
-                <TextEditor direction="bottom"></TextEditor>
-              </ResizablePanel>
-            </ResizablePanelGroup>
-          </ResizablePanel>
-        </ResizablePanelGroup>
-      </div>
-    </WebSocketProvider>
+            <EditorArea>
+              <TextEditor direction="top"></TextEditor>
+              <TextEditor direction="bottom"></TextEditor>
+            </EditorArea>
+          </AppContainer>
+        </WebSocketProvider>
+      </EditorFeatureProvider>
+    </SharingAreaProvider>
   );
+}
+
+function AppContainer({children}: {children: ReactNode}) {
+  return (
+    <div className="w-screen h-screen flex">
+      {children}
+    </div>
+
+  )
+}
+
+function EditorArea({children}: {children: ReactNode}) {
+  return (
+    <div className="w-full h-full flex flex-col">
+      {children}
+    </div>
+  )
 }
