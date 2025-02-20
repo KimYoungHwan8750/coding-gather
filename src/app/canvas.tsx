cf513daa33e024905c0113b00ee44d67ba327d15
KimYoungHwan8750 <dnfwlq123@naver.com>
2025-02-20 23:52:58 +0900

    Canvas Context API로 리팩토링

--- src/app/canvas.tsx
+++ src/app/canvas.tsx
@@ -1,80 +1,65 @@
 'use client'
 import { SearchIcon } from "lucide-react"
 import { useEffect, useRef, useState } from "react"
-import { useWs } from "./page";
+import { useWs } from "@/provider/websocket-provider";
 import { SearchMessage } from "@/lib/ws-frame-generator";
+import { useSharingArea } from "@/provider/sharing-area-provider";
 
 export default function Canvas() {
-  const [imageBitmap, setImageBitmap] = useState<ImageBitmap|null>(null);
   const nodeRef = useRef<HTMLCanvasElement>(null);
-  const [isPending, setIsPending] = useState(false);
+  const SharingArea = useSharingArea();
   const ws = useWs();
-  console.log(ws.socket.id);
-  useEffect(() => {
-      ws.socket.on("search", (res) => {
-        const parseReq = JSON.parse(res);
-      if(parseReq.code === 404) {
-        setIsPending(false);
-        alert("404 Not Found");
-      } else {
-        const byteArray = Uint8Array.from(atob(parseReq.data), c => c.charCodeAt(0));
-        const blob = new Blob([byteArray], {type: "image/png"});
-        createImageBitmap(blob).then((bitMap) => {
-          setImageBitmap(bitMap);
-          setIsPending(false);
-        })
-      }
-    })
-  }, []);
+
   useEffect(() => {
     const canvas = nodeRef.current;
     if(canvas) {
       const ctx = canvas.getContext('2d');
-      if(imageBitmap) {
-        canvas.width = imageBitmap.width;
-        canvas.height = imageBitmap.height;
+      if(SharingArea.bitmap) {
+        canvas.width = SharingArea.bitmap.width;
+        canvas.height = SharingArea.bitmap.height;
         if(ctx) {
-          ctx.drawImage(imageBitmap, 0, 0);
+          ctx.drawImage(SharingArea.bitmap, 0, 0);
         }
       }
     }
-}, [imageBitmap])
+}, [SharingArea.bitmap])
 
   return (
     <div
       className="outline outline-1 w-full h-full flex flex-col overflow-y-auto"
     >
-      <SearchBarContainer
-        setIsPending={setIsPending}
-        setImageBitmap={setImageBitmap}
-      />
-      {isPending && <div className="w-full h-full flex items-center justify-center">Loading...</div>}
-      {isPending || <canvas ref={nodeRef}></canvas>}
+      <SearchBarContainer/>
+      { SharingArea.isPending ? (
+        <div className="w-full h-full flex items-center justify-center">Loading...</div>
+      ) : (
+        <canvas ref={nodeRef}></canvas>
+      )}
     </div>
   )
 }
 
 
-function SearchBarContainer({setImageBitmap, setIsPending}: {setImageBitmap: (bitMap: ImageBitmap) => void, setIsPending: (pending: boolean) => void}) {
-  const [url, setUrl] = useState("");
+function SearchBarContainer() {
+  const SharingArea = useSharingArea();
   const ws = useWs();
   const getPage = async (url: string) => {
-    setIsPending(true);
+    SharingArea.setIsPending(true);
     ws.socket.emit("search", SearchMessage(url));
   }
   return (
     <div className="w-full h-20 shadow-md flex items-center p-5 gap-2">
-      <SearchBar setUrl={setUrl} getPage={getPage}/>
+      <SearchBar getPage={getPage}/>
       <SearchIcon
-        onClick={() => getPage(url)}
+        onClick={() => getPage(SharingArea.url)}
         className="h-8 bg-white px-2 rounded-lg box-content cursor-pointer shadow-md hover:outline-1 hover:outline hover:outline-black/20"
       />
     </div>
   )
 }
 
-function SearchBar({setUrl, getPage}: {setUrl: (str: string) => void, getPage: (url: string) => void}) {
+function SearchBar({getPage}: {getPage: (url: string) => void}) {
   const searchBarRef = useRef<HTMLTextAreaElement>(null);
+  const SharingArea = useSharingArea();
   useEffect(() => {
     if(searchBarRef.current) {
       searchBarRef.current.addEventListener("keydown", (evt) => {
@@ -89,7 +74,7 @@ function SearchBar({setUrl, getPage}: {setUrl: (str: string) => void, getPage: (
   }, [searchBarRef.current])
   return(
     <textarea ref={searchBarRef}
-      onChange={(evt) => setUrl(evt.target.value)}
+      onChange={(evt) => SharingArea.setUrl(evt.target.value)}
       className="w-full h-8 shadow outline outline-1 outline-black/20 rounded-lg resize-none leading-8 px-2 focus:shadow-around"
     />
   )
